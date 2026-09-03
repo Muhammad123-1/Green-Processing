493729ae8baf547cc5e193c5d7374154f6a15128
Muhammad <muhammadyoqubjonov7@gmail.com>
2026-09-03 15:34:33 +0500

    Fix build type error

--- src/components/inspector/ShopQCContent.tsx
+++ src/components/inspector/ShopQCContent.tsx
@@ -871,7 +871,7 @@ export default function ShopQCContent({ userRole = 'OPERATOR', userName = '', is
                   onChange={(e) => {
                     const val = e.target.value.replace(/\D/g, '');
                     if(val.length === 2 && parseInt(val) > 0 && parseInt(val) <= 31) {
-                      e.target.nextElementSibling?.nextElementSibling?.focus();
+                      (e.target.nextElementSibling?.nextElementSibling as HTMLElement)?.focus();
                     }
                     e.target.value = val;
                   }}
